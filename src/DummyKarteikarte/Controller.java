38dbec4f245689995af7595a95b399b5b34f8550
tenzens <tenzens.github@gmail.com>
2021-03-09 19:16:18 +0100

    Webview hinzugefügt, Karteikarten HTML geaddet

--- src/DummyKarteikarte/Controller.java
+++ src/DummyKarteikarte/Controller.java
@@ -1,4 +1,26 @@
 package DummyKarteikarte;
 
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Controller {
+    @FXML
+    WebView webView;
+
+    @FXML
+    public void initialize() {
+        Path filePath = Paths.get("src/Karteikarte.html");
+        try {
+            webView.getEngine().loadContent(new String(Files.readAllBytes(filePath), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
