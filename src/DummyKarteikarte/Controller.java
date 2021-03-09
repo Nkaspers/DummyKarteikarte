08ae1cc1f276473254f08ac8952c3c66b746eaf7
Ninian <n.kaspers@aol.de>
2021-03-09 23:05:35 +0100

    added choicebox to change fontsize

--- src/DummyKarteikarte/Controller.java
+++ src/DummyKarteikarte/Controller.java
@@ -1,12 +1,13 @@
 package DummyKarteikarte;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.web.WebView;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,14 +18,27 @@ public class Controller {
     public Button italicBtn;
     public Button smallerBtn;
     public Button biggerBtn;
+    public ChoiceBox<Integer> fontsizeCb;
+
+
 
     @FXML
     WebView webView;
 
     private int fontSize = 3;
 
+
+
     @FXML
     public void initialize() {
+
+        fontsizeCb.getItems().addAll(1,2,3,4,5,6,7);
+        fontsizeCb.setValue(fontSize);
+        fontsizeCb.setOnAction((actionEvent -> {
+            fontSize = fontsizeCb.getValue();
+            webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \""+ fontSize +"\");");
+        }));
+
         Path filePath = Paths.get("src/Karteikarte.html");
         try {
             webView.getEngine().loadContent(Files.readString(filePath));
@@ -53,13 +67,16 @@ public class Controller {
     private void increaseFontSize() {
         if(fontSize<7){
             webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \""+ ++fontSize +"\");");
+            fontsizeCb.setValue(fontSize);
         }
     }
     @FXML
     private void decreaseFontSize() {
         if(fontSize>1){
             webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \""+ --fontSize +"\");");
+            fontsizeCb.setValue(fontSize);
         }
+
     }
 
 }
