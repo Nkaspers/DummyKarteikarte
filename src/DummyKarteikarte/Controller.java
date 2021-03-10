9d7da32aa2c8cf996d081fbca133347948334dcc
Ninian <n.kaspers@aol.de>
2021-03-10 02:21:39 +0100

    Methode für das Ändern von Schriftarten hinzugefügt (!!)

--- src/DummyKarteikarte/Controller.java
+++ src/DummyKarteikarte/Controller.java
@@ -5,12 +5,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.web.WebView;
 
+import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Controller {
     public Button boldBtn;
@@ -19,7 +22,8 @@ public class Controller {
     public Button smallerBtn;
     public Button biggerBtn;
     public ChoiceBox<Integer> fontsizeCb;
-
+    public ColorPicker colorPicker;
+    public ChoiceBox<String> fonttypeCb;
 
 
     @FXML
@@ -28,16 +32,25 @@ public class Controller {
     private int fontSize = 3;
 
 
-
     @FXML
     public void initialize() {
 
-        fontsizeCb.getItems().addAll(1,2,3,4,5,6,7);
+        fontsizeCb.getItems().addAll(1, 2, 3, 4, 5, 6, 7);
         fontsizeCb.setValue(fontSize);
         fontsizeCb.setOnAction((actionEvent -> {
             fontSize = fontsizeCb.getValue();
-            webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \""+ fontSize +"\");");
-        }));
+            webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \"" + fontSize + "\");"); }));
+
+
+
+        fonttypeCb.getItems().addAll(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames());
+        fonttypeCb.setOnAction((actionEvent -> {
+            String fontName = fonttypeCb.getValue();
+            webView.getEngine().executeScript("document.execCommand(\"fontName\", false, \"" + fontName + "\");"); }));
+
+
+
+
 
         Path filePath = Paths.get("src/Karteikarte.html");
         try {
@@ -65,18 +78,24 @@ public class Controller {
 
     @FXML
     private void increaseFontSize() {
-        if(fontSize<7){
-            webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \""+ ++fontSize +"\");");
+        if (fontSize < 7) {
+            webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \"" + ++fontSize + "\");");
             fontsizeCb.setValue(fontSize);
         }
     }
+
     @FXML
     private void decreaseFontSize() {
-        if(fontSize>1){
-            webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \""+ --fontSize +"\");");
+        if (fontSize > 1) {
+            webView.getEngine().executeScript("document.execCommand(\"fontSize\", false, \"" + --fontSize + "\");");
             fontsizeCb.setValue(fontSize);
         }
 
     }
 
+    public void colorPicker(ActionEvent actionEvent) {
+        webView.getEngine().executeScript("document.execCommand(\"foreColor\", false, \"" + colorPicker.getValue()
+                .toString().substring(2, 8) + "\");");
+
+    }
 }
